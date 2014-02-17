8b50e7ce3a807a6c951d35f5731fb08d6759eaba
Alex Gyoshev <alex@gyoshev.net>
2014-02-17 16:58:09 +0200

    Allow deletion of entries in purchase log

--- app/model.js
+++ app/model.js
@@ -13,11 +13,48 @@ var cashwise = (function() {
         ]
     });
 
+    localDataSource.read();
+
+    function touchstart(e) {
+        var target = $(e.touch.initialTouch),
+            listview = $("[data-role=listview]").data("kendoMobileListView"),
+            model,
+            button = $(e.touch.target).find("[data-role=button]:visible");
+
+        if (target.closest("[data-role=button]")[0]) {
+            model = localDataSource.getByUid($(e.touch.target).attr("data-uid"));
+            localDataSource.remove(model);
+
+            this.events.cancel();
+            e.event.stopPropagation();
+        } else if (button[0]) {
+            button.hide();
+
+            this.events.cancel();
+        } else {
+            $(".delete[data-role=button]:visible").hide();
+        }
+    }
+
+    var swipe = function(e) {
+        var button = kendo.fx($(e.touch.currentTarget).find("[data-role=button]"));
+        button.expand().duration(200).play();
+    };
+
     return {
-        initHistory: function() {
-            var listView = this.content.find("[data-role=listview]").data("kendoMobileListView");
+        initHistory: function(e) {
+            var content = this.content;
+
+            var listView = content.find("[data-role=listview]").data("kendoMobileListView");
 
             listView.setDataSource(localDataSource);
+
+            listView.element.kendoTouch({
+                filter: ">li",
+                enableSwipe: true,
+                touchstart: touchstart,
+                swipe: swipe
+            });
         },
         closeModal: function() {
             $("[data-role=modalview]").kendoMobileModalView("close");
