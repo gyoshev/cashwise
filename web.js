98b82caee61af24d08e6245c618f3df4f5f82263
Alex Gyoshev <alex@gyoshev.net>
2014-02-22 10:36:30 +0200

    Add appcache

--- web.js
+++ web.js
@@ -12,6 +12,23 @@
     app.use(express.errorHandler());
   });
 
+  // cache manifest
+  var c = require('appcache-node');
+  var cf = c.newCache([
+    "http://code.jquery.com/jquery-1.9.1.min.js",
+    "http://cdn.kendostatic.com/2013.3.1324/js/kendo.mobile.min.js",
+    "http://cdn.kendostatic.com/2013.3.1324/styles/kendo.mobile.all.min.css",
+    "http://cdn.kendostatic.com/2013.3.1324/styles/images/kendoui.woff",
+    "localforage.min.js",
+    "model.js",
+    "styles.css"
+  ]);
+
+  app.get("/app.cache", function(req, res) {
+    res.set('Content-Type', 'text/cache-manifest');
+    res.send(200, cf);
+  });
+
   http.createServer(app).listen(app.get('port'), function(){
     console.log("Express server listening on port " + app.get('port'));
   });
